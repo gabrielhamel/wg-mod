59d853a90f457a0d2ac3a1357c80fb63d3d2e75e
Gabouchet <gabriel.hamel.pro@gmail.com>
2023-08-23 12:27:12 +0200

    wip

---
+++ src/config.rs
@@ -0,0 +1,27 @@
+use serde_derive::Deserialize;
+use std::path::PathBuf;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("Unable to find the user home directory")]
+    UserHomeError,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Configs {
+    pub wg_mod_home: PathBuf,
+}
+
+fn get_tool_home() -> Result<PathBuf, Error> {
+    let user_path: std::path::PathBuf = home::home_dir().ok_or(Error::UserHomeError)?;
+    let wg_tool_path = user_path.join(".wg-mod");
+    Ok(wg_tool_path)
+}
+
+impl Configs {
+    pub fn load() -> Result<Self, Error> {
+        Ok(Configs {
+            wg_mod_home: get_tool_home()?,
+        })
+    }
+}
