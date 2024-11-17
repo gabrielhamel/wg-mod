aa4ccd0c3d6a6278c0aa7788296eaeed9f64576e
Gabriel Hamel <gabriel.hamel.pro@gmail.com>
2024-11-17 16:58:41 +0100

    test node installation

--- src/sdk/node/linux_or_macos.rs
+++ src/sdk/node/linux_or_macos.rs
@@ -1,6 +1,8 @@
-use crate::sdk::node::Node;
+use crate::sdk::node::{Node, NodeError};
 use crate::sdk::npm::NPM;
+use crate::utils::command::command;
 use std::path::PathBuf;
+use std::process::Output;
 
 pub struct LinuxOrMacNode {
     node_path: PathBuf,
@@ -15,5 +17,15 @@ impl From<PathBuf> for LinuxOrMacNode {
 impl Node for LinuxOrMacNode {
     fn get_npm(&self) -> NPM {
         NPM::from(self.node_path.join("bin").join("npm"))
+    }
+
+    fn exec(&self, args: Vec<&str>) -> Result<Output, NodeError> {
+        let binaries_path = self.node_path.join("bin");
+        let node_exec_path = binaries_path.join("node");
+
+        let executable =
+            node_exec_path.to_str().ok_or(NodeError::FailedExecution)?;
+        command(executable, args, vec![])
+            .map_err(|_| NodeError::FailedExecution)
     }
 }
