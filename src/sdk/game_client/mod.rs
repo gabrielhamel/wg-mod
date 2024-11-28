a5f6148b339a26162fbea5d337bfbfb30a80fe88
cedran <cedran.bastien1@etu.univ-lorraine.fr>
2024-11-28 16:26:57 +0100

    refacto: Refactor settings implementation

---
+++ src/sdk/game_client/mod.rs
@@ -0,0 +1,11 @@
+use std::path::PathBuf;
+
+pub struct GameClient {
+    pub path: PathBuf,
+}
+
+impl From<PathBuf> for GameClient {
+    fn from(path: PathBuf) -> Self {
+        GameClient { path }
+    }
+}
