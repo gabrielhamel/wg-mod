6d44581e4352d698eda2027afa2299f415666940
Cedran bastien <cedran.bastien@orange.fr>
2024-12-20 15:46:59 +0100

    fix: Keep directory architecture when doing file copy

--- src/utils/copy_directory.rs
+++ src/utils/copy_directory.rs
@@ -1,6 +1,7 @@
+use std::fs::read_dir;
 use fs_extra::dir::{get_dir_content, CopyOptions};
 use std::path::PathBuf;
-use std::result;
+use std::{io, result};
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
@@ -8,16 +9,19 @@ pub enum Error {
     CopyDirectoryError(PathBuf, PathBuf, fs_extra::error::Error),
 
     #[error("Cannot read the directory")]
-    GetDirectoryContentError(#[from] fs_extra::error::Error),
+    GetDirectoryContentError(#[from] io::Error),
 }
 
 type Result<T> = result::Result<T, Error>;
 
 pub fn copy_directory(source: &PathBuf, destination: &PathBuf) -> Result<()> {
     let options = CopyOptions::new();
-    let content = get_dir_content(source)?;
+    let dir_content = read_dir(source)?
+        .flatten()
+        .map(|entry| entry.path())
+        .collect::<Vec<_>>();
 
-    fs_extra::copy_items(&content.files, destination.as_path(), &options)
+    fs_extra::copy_items(&dir_content, destination.as_path(), &options)
         .map_err(|e| {
             Error::CopyDirectoryError(source.clone(), destination.clone(), e)
         })?;
